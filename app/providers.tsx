25c94de4ab308a640c5ad177b0ed6dd94bfa0ed1
serenacancanguo-png <serena.cancanguo@gmail.com>
2026-02-11 15:10:06 +1100

    Use ReactNode type from 'react' in Providers

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,7 +1,8 @@
 'use client'
 
 import { SessionProvider } from 'next-auth/react'
+import type { ReactNode } from 'react'
 
-export default function Providers({ children }: { children: React.ReactNode }) {
+export default function Providers({ children }: { children: ReactNode }) {
   return <SessionProvider>{children}</SessionProvider>
 }
